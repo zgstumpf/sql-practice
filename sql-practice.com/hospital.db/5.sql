cbc828bccd55471ab4fd15952f54f4c9368220a1
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-10-03 14:34:55 -0400

    Create 5.sql

---
+++ sql-practice.com/hospital.db/5.sql
@@ -0,0 +1,6 @@
+-- Update the patients table for the allergies column. 
+-- If the patient's allergies is null then replace it with 'NKA'
+
+UPDATE patients
+SET allergies = 'NKA'
+WHERE allergies IS NULL;
